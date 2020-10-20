509d572edf48582443168a4d3742e1241a4faed7
Davi Freitas da Silva <davi.frrs@outlook.com>
2020-10-20 11:45:54 -0300

    Delete button working

--- client/src/components/ListTodos.tsx
+++ client/src/components/ListTodos.tsx
@@ -1,12 +1,27 @@
 import React, { Fragment, useState, useEffect } from "react";
 
 interface toDo {
-  id: number;
+  todo_id: number;
   description: string;
 }
 
 const ListTodos = () => {
   const [todos, setTodos] = useState([]);
+
+  //   DELETE TODO FUNCTION
+  async function deleteTodo(id: number) {
+    try {
+      const res = await fetch(`http://localhost:5000/todos/${id}`, {
+        method: "DELETE",
+      });
+
+      setTodos(todos.filter((todo: toDo) => todo.todo_id !== id));
+    } catch (err) {
+      console.error(err.message);
+    }
+  }
+
+  //   GET TODOS FUNCTION
 
   async function getTodos() {
     const response = await fetch("http://localhost:5000/todos");
@@ -34,10 +49,17 @@ const ListTodos = () => {
         </thead>
         <tbody>
           {todos.map((todo: toDo) => (
-            <tr key={todo.id}>
+            <tr key={todo.todo_id}>
               <td>{todo.description}</td>
               <td>Edit</td>
-              <td>Delete</td>
+              <td>
+                <button
+                  className="btn btn-danger"
+                  onClick={() => deleteTodo(todo.todo_id)}
+                >
+                  Delete
+                </button>
+              </td>
             </tr>
           ))}
         </tbody>
